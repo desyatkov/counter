194dd0cd04a28136633222ff7207b32659c66713
Sergey Desyatkov <Desyatkov@gmail.com>
2018-05-13 13:35:42 +0300

    init

---
+++ games.js
@@ -0,0 +1,128 @@
+module.exports = [
+  {
+    group:'Russia v Saudi Arabia',
+    time: '2018-06-14 15:00:00 UTC',
+    id: 100,
+    left: 'Russia',
+    right: 'Saudi Arabia'
+  },
+  {
+    group:'Portugal v Spain',
+    time: '2018-06-15 18:00:00 UTC',
+    id: 101,
+    left: 'Portugal',
+    right: 'Spain'
+  },
+  {
+    group:'Argentina vs Iceland',
+    time: '2018-06-16 13:00:00 UTC',
+    id: 102,
+    left: 'Argentina',
+    right: 'Iceland'
+  },
+  {
+    group:'Germany v Mexico',
+    time: '2018-06-17 15:00:00 UTC',
+    id: 103,
+    left: 'Germany',
+    right: 'Mexico'
+  },
+  {
+    group:'Brazil v Switzerland',
+    time: '2018-06-17 18:00:00 UTC',
+    id: 104,
+    left: 'Brazil',
+    right: 'Switzerland'
+  },
+  {
+    group:'Belgium v Panama',
+    time: '2018-06-18 15:00:00 UTC',
+    id: 105,
+    left: 'Belgium',
+    right: 'Panama'
+  },
+  {
+    group:'Poland v. Senegal',
+    time: '2018-06-19 15:00:00 UTC',
+    id: 106,
+    left: 'Poland',
+    right: 'Senegal'
+  },
+  {
+    group:'Russia v Egypt',
+    time: '2018-06-19 18:00:00 UTC',
+    id: 107,
+    left: 'Russia',
+    right: 'Egypt'
+  },
+  {
+    group:'Argentina v Croatia',
+    time: '2018-06-21 18:00:00 UTC',
+    id: 108,
+    left: 'Argentina',
+    right: 'Croatia'
+  },
+  {
+    group:'Brazil v Costa Rica',
+    time: '2018-06-22 12:00:00 UTC',
+    id: 109,
+    left: 'Brazil',
+    right: 'Costa Rica'
+  },
+  {
+    group:'Belgium v Tunisia',
+    time: '2018-06-23 12:00:00 UTC',
+    id: 110,
+    left: 'Belgium',
+    right: 'Tunisia'
+  },
+  {
+    group:'Germany v Sweden',
+    time: '2018-06-23 18:00:00 UTC',
+    id: 111,
+    left: 'Germany',
+    right: 'Sweden'
+  },
+  {
+    group:'Poland v Colombia',
+    time: '2018-06-24 18:00:00 UTC',
+    id: 112,
+    left: 'Poland',
+    right: 'Colombia'
+  },
+  {
+    group:'Spain v Morocco',
+    time: '2018-06-25 18:00:00 UTC',
+    id: 113,
+    left: 'Spain',
+    right: 'Morocco'
+  },
+  {
+    group:'Nigeria v Argentina',
+    time: '2018-06-26 18:00:00 UTC',
+    id: 114,
+    left: 'Nigeria',
+    right: 'Argentina'
+  },
+  {
+    group:'Germany v South Korea',
+    time: '2018-06-27 14:00:00 UTC',
+    id: 115,
+    left: 'Germany',
+    right: 'South Korea'
+  },
+  {
+    group:'Serbia v Brazil',
+    time: '2018-06-27 18:00:00 UTC',
+    id: 116,
+    left: 'Serbia',
+    right: 'Brazil'
+  },
+  {
+    group:'Japan v Poland',
+    time: '2018-06-28 14:00:00 UTC',
+    id: 117,
+    left: 'Japan',
+    right: 'Poland'
+  }
+];
---
+++ index.js
@@ -0,0 +1,58 @@
+var schedule = require('./games');
+
+var compose = function compose() {
+  for (var _len = arguments.length, fns = Array(_len), _key = 0; _key < _len; _key++) {
+    fns[_key] = arguments[_key];
+  }
+
+  return function (x) {
+    return fns.reduceRight(function (acc, fn) {
+      return fn(acc);
+    }, x);
+  };
+};
+
+// return game status 'outdated' and 'game over' (+2h from start)
+function gameStatus(diffMs) {
+  var TWO_HOURS = -7200001;
+  var outdated = diffMs <= 0 ;
+  var gameOver = diffMs <= TWO_HOURS;
+  return { outdated: outdated, gameOver: gameOver }
+}
+
+// return diff from current to deadline
+function getDiff(time){
+  var exp = new Date('2018-06-18 20:01:00'); //?
+  var now = new Date(exp.getFullYear(), exp.getMonth(), exp.getDate(), exp.getHours(), exp.getMinutes(), 0).valueOf();
+  var target = new Date(time).valueOf();
+  return  target - now;
+}
+
+// return object find relevant
+function timeObject(diffMs) {
+  var day, hour, minute, seconds;
+  seconds = Math.floor(diffMs / 1000);
+  minute = Math.floor(seconds / 60);
+  hour = Math.floor(minute / 60);
+  minute = minute % 60;
+  day = Math.floor(hour / 24);
+  hour = hour % 24;
+  var status = gameStatus(diffMs);
+  return { day: day, hour: hour, minute: minute, outdated: status.outdated, gameOver: status.gameOver };
+}
+
+// find object relevant game
+function findGame( gamesList ){
+  var statusCompose = compose(gameStatus, getDiff);
+
+  for(var i = 0; i <= gamesList.length; i++){
+    var status = statusCompose(gamesList[i].time);
+    if ( !status.outdated && !status.gameOver || status.outdated && !status.gameOver  ) {
+      return gamesList[i]
+    }
+  }
+}
+
+var game = findGame( schedule );                        //?. $
+var resObj = compose(timeObject, getDiff)( game.time ); //?. $
+
